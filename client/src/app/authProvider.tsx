5d2966f148ca8d9df4d573a9f47186a74363cbd7
Ian Anderson <ian.anderson.19@cnu.edu>
2024-11-14 14:29:14 -0500

    fixed authentication issue

--- client/src/app/authProvider.tsx
+++ client/src/app/authProvider.tsx
@@ -19,28 +19,25 @@ const formFields = {
       order: 1,
       placeholder: "Choose a username",
       label: "Username",
-      inputProps: { required: true },
     },
     email: {
-      order: 1,
+      order: 2,
       placeholder: "Enter your email address",
       label: "Email",
-      inputProps: { type: "email", required: true },
     },
     password: {
       order: 3,
       placeholder: "Enter your password",
       label: "Password",
-      inputProps: { type: "password", required: true },
     },
     confirm_password: {
       order: 4,
       placeholder: "Confirm your password",
       label: "Confirm Password",
-      inputProps: { type: "password", required: true },
     },
   },
 };
+
 
 const AuthProvider = ({ children }: any) => {
   return (
